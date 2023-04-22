72ea89de053f3e10bafb7304542d086463785608
king <mattends@gmail.com>
2023-04-22 15:22:22 +0000

    my second script

---
+++ red.sh
@@ -0,0 +1,20 @@
+#! /bin/bash/
+echo "What is your name?"
+read name
+echo "What is the name of the Company?"
+read company
+echo "What is your new position?"
+read position
+echo "What is your preferred name?"
+read preferred
+echo "What is your marital Status?"
+read status
+echo "If married state your spouse name"
+read spouse
+echo
+echo
+echo "Hello $name"
+echo "Welcome to $company"
+echo "As a $position we would love to walk you through our 3 week orientation"
+echo "$preferred our team here cannot wait to meet you"
+echo "I hope $spouse and the family are doing well"
